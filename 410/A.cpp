047f9416dc3273b1d48206331640c244670d74c5
xfcherish <xiongfeng@recreatemyself.local>
2017-04-17 20:22:35 +0800

    410

---
+++ 410/A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+# define pb push_back
+# define mp make_pair
+
+typedef long long LL;
+typedef pair<int,int> PII;
+
+const int maxn =  105;
+
+int main()
+{
+
+	return 0;
+}
