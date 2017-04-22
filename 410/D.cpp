04a90af7d9802f658bd33607c1c5ad0b6f45d397
xfcherish <messiforever10@gmail.com>
2017-04-22 17:59:56 +0800

    solve 410D

--- 410/D.cpp
+++ 410/D.cpp
@@ -19,8 +19,31 @@ typedef pair<int,int> PII;
 
 const int maxn =  (1e5)+10;
 
+int a[maxn],b[maxn],p[maxn],n;
+
+bool cmp(const int & s1, const int & s2) {
+	return a[s1] > a[s2];
+}
+
 int main()
 {
-	int a;
+	cin >> n;
+	for(int i = 1; i <= n; i++) cin >> a[i];
+	for(int i = 1; i <= n; i++) cin >> b[i];
+	for(int i = 1; i <= n; i++) p[i] = i;
+	sort(p+1, p+1+n, cmp);
+	vector<int> ans;
+	ans.push_back(p[1]);
+	int k = (n/2) + 1;
+	for(int i = 2; i <= n; i += 2) {
+		int cur = p[i];
+		if(i+1 <= n && b[cur] < b[p[i+1]])
+			cur = p[i+1];
+		ans.push_back(cur);
+	}
+	cout << k << endl;
+	for(int i = 0; i < ans.size(); i++)
+		cout << ans[i] << " ";
+	cout << endl;
 	return 0;
 }
